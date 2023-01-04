2517911f5a94150fffc139562e576dbdec02457a
Pavel4090 <pavelcurikov090@gmail.com>
2023-01-04 22:03:58 +0300

    Fixing

--- src/homework6/CreditCard.java
+++ src/homework6/CreditCard.java
@@ -7,7 +7,7 @@ public class CreditCard {
     Integer accountNumber;
     double summaOnAccount;
 
-    void cardplus() {
+    public void card() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер счёта");
         accountNumber = sc.nextInt();
@@ -15,27 +15,22 @@ public class CreditCard {
         summaOnAccount = sc.nextDouble();
         System.out.println("Номер счёта " + accountNumber);
         System.out.println("Сумма на счету " + summaOnAccount);
+    }
+    public void cardplus() {
         System.out.println("Введите сумму для пополнения баланса");
         Scanner sx = new Scanner(System.in);
         double z = sx.nextDouble();
         summaOnAccount = summaOnAccount + z;
     }
 
-    void cardminus() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите номер счёта");
-        accountNumber = sc.nextInt();
-        System.out.println("Введите сумму");
-        summaOnAccount = sc.nextDouble();
-        System.out.println("Номер счёта " + accountNumber);
-        System.out.println("Сумма на счету " + summaOnAccount);
+    public void cardminus() {
         System.out.println("Введите сумму для снятия денег ");
         Scanner sx = new Scanner(System.in);
         double z = sx.nextDouble();
         summaOnAccount = summaOnAccount - z;
     }
 
-    void vol() {
+    public void vol() {
         System.out.println("Номер Счёта " + accountNumber);
         System.out.println("Сумма на счету " + summaOnAccount);
     }
--- src/homework6/CreditCard1.java
+++ src/homework6/CreditCard1.java
@@ -5,10 +5,13 @@ public class CreditCard1 {
         CreditCard card1 = new CreditCard();
         CreditCard card2 = new CreditCard();
         CreditCard card3 = new CreditCard();
+        card1.card();
         card1.cardplus();
         card1.vol();
+        card2.card();
         card2.cardplus();
         card2.vol();
+        card3.card();
         card3.cardminus();
         card3.vol();
     }
