79466a6cfb7fb7060e65869a36e70eddfb8798a1
Pavel4090 <pavelcurikov090@gmail.com>
2023-01-04 16:47:36 +0300

    Fixing

--- src/homework6/CreditCard.java
+++ src/homework6/CreditCard.java
@@ -1,16 +1,42 @@
 package homework6;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class CreditCard {
-    Integer AccountNumber;
-    double SummaOnAccount;
-    void vol1 () {
-        for (String s : Arrays.asList("Номер счета " + AccountNumber, "Сумма на счету " + SummaOnAccount)) {
-            System.out.println(s);
-        }
+
+    Integer accountNumber;
+    double summaOnAccount;
+
+    void cardplus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите номер счёта");
+        accountNumber = sc.nextInt();
+        System.out.println("Введите сумму");
+        summaOnAccount = sc.nextDouble();
+        System.out.println("Номер счёта " + accountNumber);
+        System.out.println("Сумма на счету " + summaOnAccount);
+        System.out.println("Введите сумму для пополнения баланса");
+        Scanner sx = new Scanner(System.in);
+        double z = sx.nextDouble();
+        summaOnAccount = summaOnAccount + z;
     }
-    void vol2 () {
-        System.out.println("Сумма на счету" + SummaOnAccount);
+
+    void cardminus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите номер счёта");
+        accountNumber = sc.nextInt();
+        System.out.println("Введите сумму");
+        summaOnAccount = sc.nextDouble();
+        System.out.println("Номер счёта " + accountNumber);
+        System.out.println("Сумма на счету " + summaOnAccount);
+        System.out.println("Введите сумму для снятия денег ");
+        Scanner sx = new Scanner(System.in);
+        double z = sx.nextDouble();
+        summaOnAccount = summaOnAccount - z;
+    }
+
+    void vol() {
+        System.out.println("Номер Счёта " + accountNumber);
+        System.out.println("Сумма на счету " + summaOnAccount);
     }
 }
